b28414232af5bb9021337b7f389f5cd630c1dc31
arulh <arul.howard@telus.net>
2025-03-31 03:20:55 -0700

    added load balancer

--- client.py
+++ client.py
@@ -1,21 +1,25 @@
-import requests
-
-url = "http://127.0.0.1:8000/batch_inference"
-
-# Prepare the image files
-# files = [
-#     ('files', ('apple.jpg', open('apple.jpg', 'rb'), 'image/jpeg')),
-#     ('files', ('banana.jpg', open('banana.jpg', 'rb'), 'image/jpeg')),
-# ] * 10
-
-files = []
-for _ in range(1):  # Send each file 10 times properly
-    files.append(('files', ('apple.jpg', open('apple.jpg', 'rb'), 'image/jpeg')))
-    files.append(('files', ('banana.jpg', open('banana.jpg', 'rb'), 'image/jpeg')))
+import asyncio
+import httpx
 
 
-# Send the request
-response = requests.post(url, files=files)
+lan_servers = ["http://10.0.0.100:8000/batch_inference", "http://127.0.0.1:8000/batch_inference"]
 
-# Print the response
-print(response.json())
+
+async def load_balancer(servers):
+    files = []
+    for _ in range(500): # send two sample images 500 times
+        files.append(('files', ('apple.jpg', open('apple.jpg', 'rb'), 'image/jpeg')))
+        files.append(('files', ('banana.jpg', open('banana.jpg', 'rb'), 'image/jpeg')))
+
+    async with httpx.AsyncClient() as client:
+        server1 = client.post(lan_servers[0], files=files[:len(files)//2])
+        server2 = client.post(lan_servers[1], files=files[len(files)//2:])
+
+        response1, response2 = await asyncio.gather(server1, server2)
+
+        print(response1.json())
+        print(response2.json())
+
+
+if __name__ == "__main__":
+    asyncio.run(load_balancer(lan_servers))
